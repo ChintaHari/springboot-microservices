f8278c4b4bac8da9b6edeb06f2886d75ded4fd62
Hari Krishna Chinta <hkharikrishna62@gmail.com>
2024-08-07 18:17:25 -0500

    (DOESN'T_WORK_JUST_FOR_REEFRENCE) Added fallback controller in cloud-api-gateway

--- cloud-api-gateway/src/main/java/com/example/cloud_api_gateway/CloudApiGatewayApplication.java
+++ cloud-api-gateway/src/main/java/com/example/cloud_api_gateway/CloudApiGatewayApplication.java
@@ -3,9 +3,12 @@ package com.example.cloud_api_gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableHystrix
 public class CloudApiGatewayApplication {
 
 	public static void main(String[] args) {
