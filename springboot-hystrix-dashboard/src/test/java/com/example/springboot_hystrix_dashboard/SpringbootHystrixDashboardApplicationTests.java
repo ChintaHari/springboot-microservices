b116ef944e0710bb2b46bcc76d4b190b58219217
Hari Krishna Chinta <hkharikrishna62@gmail.com>
2024-08-07 18:18:31 -0500

    (DOESN'T_WORK_JUST_FOR_REEFRENCE) Hystrix dashboard springboot project

---
+++ springboot-hystrix-dashboard/src/test/java/com/example/springboot_hystrix_dashboard/SpringbootHystrixDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_hystrix_dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootHystrixDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
