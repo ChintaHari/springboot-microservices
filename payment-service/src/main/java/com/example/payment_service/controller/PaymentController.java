83d0e55e21e874ebc3b808028c2b5cfb1dd737b8
Hari Krishna Chinta <hkharikrishna62@gmail.com>
2024-08-07 17:42:36 -0500

    logging for payment-service

--- payment-service/src/main/java/com/example/payment_service/controller/PaymentController.java
+++ payment-service/src/main/java/com/example/payment_service/controller/PaymentController.java
@@ -1,6 +1,7 @@
 package com.example.payment_service.controller;
 
 import java.util.Optional;
+import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.payment_service.entity.Payment;
 import com.example.payment_service.service.PaymentService;
+import com.fasterxml.jackson.core.JsonProcessingException;
 
 @RestController
 @RequestMapping("/payment")
@@ -20,14 +22,19 @@ public class PaymentController {
     @Autowired
     private PaymentService paymentService;
 
+    private Logger logger = Logger.getLogger(PaymentService.class.getName());
+
+
     @PostMapping("/doPayment")
-    public Payment doPayment(@RequestBody Payment payment) {
+    public Payment doPayment(@RequestBody Payment payment) throws JsonProcessingException {
         System.out.println("Inside doPayment of PaymentController");
         return paymentService.doPayment(payment);
     }
 
     @GetMapping("/getPaymentByOrderId/{orderId}")
     public Optional<Payment> getPaymentByOrderId(@PathVariable int orderId) {
+        Payment payment = paymentService.getPaymentByOrderId(orderId).get();
+        logger.info("Payment-Serivce getPaymentByOrderId : " + payment);
         return paymentService.getPaymentByOrderId(orderId);
     }
     
--- payment-service/src/main/java/com/example/payment_service/service/PaymentService.java
+++ payment-service/src/main/java/com/example/payment_service/service/PaymentService.java
@@ -3,12 +3,16 @@ package com.example.payment_service.service;
 import java.util.Optional;
 import java.util.Random;
 import java.util.UUID;
+import java.util.logging.Logger;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.payment_service.entity.Payment;
 import com.example.payment_service.repository.PaymentRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
 public class PaymentService {
@@ -16,9 +20,14 @@ public class PaymentService {
     @Autowired
     private PaymentRepository paymentRepository;
 
-    public Payment doPayment(Payment payment) {
+    private Logger logger = Logger.getLogger(PaymentService.class.getName());
+
+
+    public Payment doPayment(Payment payment) throws JsonProcessingException {
         payment.setPaymentStatus(paymentProcessing());
         payment.setTransactionId(UUID.randomUUID().toString());
+
+        logger.info("Payment-Serivce request : " + new ObjectMapper().writeValueAsString(payment));
         return paymentRepository.save(payment);
     }
 
