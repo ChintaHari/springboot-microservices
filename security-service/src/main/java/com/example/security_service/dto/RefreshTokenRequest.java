c7963cca8476e19b9bc1a2a88f9c75246e8ec540
Hari Krishna Chinta <hkharikrishna62@gmail.com>
2024-09-05 16:19:30 -0400

    Added Refresh Token login and created images for the test flow

--- security-service/src/main/java/com/example/security_service/dto/RefreshTokenRequest.java
+++ security-service/src/main/java/com/example/security_service/dto/RefreshTokenRequest.java
@@ -1,4 +1,4 @@
-package com.example.security_service.entity;
+package com.example.security_service.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -7,7 +7,6 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class AuthRequest {
-    public String username;
-    public String password;
+public class RefreshTokenRequest {
+    private String token;
 }
